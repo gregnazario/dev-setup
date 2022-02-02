df017a9d4aaa3dd989a11e7a0195a45ee11867c0
Greg Nazario <greg@gnazar.io>
2022-02-01 22:21:11 -0800

    [git] Push update-main script to bin folder

--- git/install.sh
+++ git/install.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-BASH_CONFIG="/home/$USER/.bashrc"
+BASH_CONFIG="$HOME/.bashrc"
 
+# Determine whether to use bash_profile instead on Mac
 if [ `uname -s` == 'Darwin' ]; then
-  BASH_CONFIG="/Users/$USER/.bash_profile"
+  BASH_CONFIG="$HOME/.bash_profile"
 fi
+
+# Copy update script over
+mkdir $HOME/bin
+cp update-main.sh $HOME/bin/update-main.sh
+
+# Install Aliases
 echo "alias update-branch='git rebase -i main'
 alias update-main='bash ~/bin/update-main.sh'" >> $BASH_CONFIG
+
